8b950447699c64d5e8658a92a36d25b41e498a51
darkstreet <55440418+Darkstreet00@users.noreply.github.com>
2021-03-03 13:55:48 +0100

    Add files via upload

---
+++ solus.sh
@@ -0,0 +1,25 @@
+sudo eopkg -y update-repo
+sudo eopkg -y upgrade
+sudo eopkg -y install gcc discord python3 python3-devel curl wget eom git make
+sudo eopkg -y it -c system.devel
+
+#sudo mkdir /opt
+#sudo chown kriss:kriss /opt
+
+wget -O /opt/ipmitool.zip https://github.com/ipmitool/ipmitool/archive/master.zip
+cd /opt/
+unzip ipmitool.zip
+cd /opt/ipmitool-master
+bash bootstrap && bash configure && make && sudo make install
+ln -s /usr/local/bin/ipmitool /usr/bin
+rm /opt/ipmitool.zip
+
+cd /opt/
+wget -O /opt/pyipmi.zip https://github.com/Darkstreet00/pyipmi/archive/main.zip
+unzip pyipmi.zip
+cd /opt/pyipmi-main
+mv ipmi.py pyipmi
+chmod +x pyipmi
+ln -s /opt/pyipmi-main/pyipmi /usr/bin
+pip3 install bcrypt
+cd
