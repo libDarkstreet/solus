b0925ac0babaca09496558f777f2d4e144982a1d
darkstreet <55440418+Darkstreet00@users.noreply.github.com>
2021-03-08 15:09:40 +0100

    Update solus.sh

--- solus.sh
+++ solus.sh
@@ -2,6 +2,7 @@ sudo eopkg -y update-repo
 sudo eopkg -y upgrade
 sudo eopkg -y install gcc discord python3 python3-devel curl wget eom git make scrot
 sudo eopkg -y it -c system.devel
+sudo eopkg install libelf-devel linux-current-headers linux-headers
 
 sudo mkdir /opt
 sudo chown kriss:kriss /opt
