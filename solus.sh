be5ffe42101434c79111cf61c09e665fe171288d
Kriss <55440418+libDarkstreet@users.noreply.github.com>
2022-11-04 17:37:45 +0100

    Update solus.sh

--- solus.sh
+++ solus.sh
@@ -3,4 +3,3 @@ sudo eopkg -y upgrade
 sudo eopkg -y install gcc discord python3 python3-devel curl wget eom git make scrot
 sudo eopkg -y it -c system.devel
 sudo eopkg install libelf-devel linux-current-headers linux-headers
-sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
