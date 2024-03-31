abc306d3a318713d41bec5341b670eb33adbbc39
Azuros <55440418+libDarkstreet@users.noreply.github.com>
2024-03-31 03:34:17 +0200

    Update solus.sh

--- solus.sh
+++ solus.sh
@@ -1,5 +1,9 @@
 sudo eopkg -y update-repo
 sudo eopkg -y upgrade
-sudo eopkg -y install gcc discord python3 python3-devel curl wget eom git make scrot
+sudo eopkg -y install gcc discord python3 python3-devel curl wget git make
 sudo eopkg -y it -c system.devel
 sudo eopkg install libelf-devel linux-current-headers linux-headers
+
+sudo eopkg install docker
+sudo systemctl enable docker.service --now
+sudo usermod -aG docker $USER
