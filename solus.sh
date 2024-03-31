ab5733f5dafc9dcbb16dd48422ab8dacb225ee21
Azuros <55440418+libDarkstreet@users.noreply.github.com>
2024-03-31 03:39:28 +0200

    Update solus.sh

--- solus.sh
+++ solus.sh
@@ -1,6 +1,6 @@
 sudo eopkg -y update-repo
 sudo eopkg -y upgrade
-sudo eopkg -y install gcc discord python3 python3-devel curl wget git make vlc
+sudo eopkg -y install gcc discord python3 python3-devel curl wget git make vlc gnome-tweaks
 sudo eopkg -y it -c system.devel
 sudo eopkg install libelf-devel linux-current-headers linux-headers
 
