a10d3900cac0b8e071920337bfea7a18065a760b
Azuros <55440418+libDarkstreet@users.noreply.github.com>
2024-04-01 14:50:21 +0200

    Update solus.sh

--- solus.sh
+++ solus.sh
@@ -4,7 +4,7 @@ sudo eopkg -y install gcc discord python3 python3-devel curl wget git make vlc g
 sudo eopkg -y it -c system.devel
 sudo eopkg install libelf-devel linux-current-headers linux-headers
 
-sudo eopkg install docker
+sudo eopkg install docker docker-compose
 sudo systemctl enable docker.service --now
 sudo usermod -aG docker $USER
 
